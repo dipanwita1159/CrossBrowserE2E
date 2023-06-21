3b01de5f1ff82cb355f7c44f8e579e8deca027e2
dipanwita1159 <cdipanwita04@gmail.com>
2023-06-21 17:21:00 +0530

    first commit

---
+++ src/main/java/com/mohs10/base/StartBrowser.java
@@ -0,0 +1,77 @@
+package com.mohs10.base;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
+
+
+public class StartBrowser {
+	 public static WebDriver driver;
+	 //set up report
+	 public static ExtentReports extent;
+	 public static ExtentTest parentTest;
+	 public static ExtentTest childTest;
+	 ExtentSparkReporter sparkReporter;
+	 
+	 @BeforeTest
+	 public void generateReport()
+	 {
+		 sparkReporter = new ExtentSparkReporter("Report/AutomationReport.html");
+		 extent = new ExtentReports();
+		 extent.attachReporter(sparkReporter);
+	 }
+	 
+	 @BeforeMethod
+	 public void methodName(Method method)
+	 {
+		parentTest = extent.createTest(method.getName()); 
+	 }
+	 @Test
+	 public static WebDriver beforeClass(String browser, String url) {
+	     
+	     if (browser.equalsIgnoreCase("firefox")) {
+	         WebDriverManager.firefoxdriver().setup();
+	         driver = new FirefoxDriver();
+	     } else if (browser.equalsIgnoreCase("chrome")) {
+	         WebDriverManager.chromedriver().setup();
+	         driver = new ChromeDriver();
+	     } else if (browser.equalsIgnoreCase("edge")) {
+	        WebDriverManager.edgedriver().setup();
+	         driver = new EdgeDriver();
+	     } else {
+	         throw new IllegalArgumentException("Invalid browser specified: " + browser);
+	     }
+	     
+	     driver.manage().window().maximize();
+	     driver.get(url);
+	     
+	     return driver;
+	 }
+
+	  
+
+		
+		  //@AfterClass public void afterClass() { driver.quit(); extent.flush(); }
+		 
+
+	}
---
+++ src/test/java/com/mohs10/scripts/HomeTest.java
@@ -0,0 +1,43 @@
+package com.mohs10.scripts;
+
+import org.testng.annotations.Test;
+
+import com.mohs10.ActionDriver.SSreuseclass;
+import com.mohs10.ActionDriver.XLUtils;
+import com.mohs10.base.StartBrowser;
+
+import com.mohs10.reuse.CommonFuns;
+
+public class HomeTest extends StartBrowser {
+
+	String excelfile = "C:\\My Documents\\a\\trainning season\\CrossBrowser_painite\\TestData\\Data.xlsx";
+	String excelsheet = "Login";
+
+	@Test
+	public void chrome() throws Exception {
+
+		CommonFuns coms = new CommonFuns();
+		int rowcount = XLUtils.getRowCount(excelfile, excelsheet);
+		for (int i = 1; i < rowcount; i++) {
+			String Email = XLUtils.getStringCellData(excelfile, excelsheet, i, 0);
+			String Pwd = XLUtils.getStringCellData(excelfile, excelsheet, i, 1);
+			String url = XLUtils.getStringCellData(excelfile, excelsheet, i, 2);
+			String browser = XLUtils.getStringCellData(excelfile, excelsheet, i, 3);
+
+			driver = StartBrowser.beforeClass(browser, url);
+            System.out.println(browser + " :this is the opened browser");
+			Thread.sleep(2000);
+			coms.logIn(Email, Pwd);
+            coms.Categories();
+			coms.Blogbtn();
+			coms.Newsbtn();
+			// coms.logout();
+			System.out.println(browser + " :current browser closed");
+			Thread.sleep(2000);
+
+			SSreuseclass.SSReusablemethod(driver, "Screenshoot:"+i);
+			driver.close();
+
+		}
+	}
+}
